05177cf16a0bcb66f4828efce42c3ec0166ddb7b
Tetsiya Nishimura <ttynsmr@gmail.com>
2020-11-14 14:43:30 +0900

    Formのドラッグでウィンドウを移動できるようにした

--- Form1.cs
+++ Form1.cs
@@ -22,7 +22,21 @@ namespace LiveFrame
             notifyIcon.Click += (sender, e) => ToggleEditMode();
             notifyIcon.DoubleClick += (sender, e) => Application.Exit();
 
-            Click += (sender, e) => ToggleEditMode();
+            Click += (sender, e) => {
+                if (((MouseEventArgs)e).Button == MouseButtons.Right)
+                {
+                    ToggleEditMode();
+                }
+            };
+
+            MouseDown += (sender, e) =>
+            {
+                if (e.Button == MouseButtons.Left)
+                {
+                    Win32.ReleaseCapture();
+                    Win32.SendMessage(Handle, Win32.WM_NCLBUTTONDOWN, Win32.HT_CAPTION, 0);
+                }
+            };
 
             FormBorderStyle = FormBorderStyle.Sizable;
             Opacity = 0.25;
