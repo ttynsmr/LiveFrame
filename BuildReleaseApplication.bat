0aa5d5d4c54cc38ae101f4c15f358d43f13bdc8f
Tetsuya Nishimura <ttynsmr@gmail.com>
2021-12-09 00:41:27 +0900

    ビルドを `dotnet publish` にする

--- BuildReleaseApplication.bat
+++ BuildReleaseApplication.bat
@@ -3,4 +3,5 @@ setlocal enabledelayedexpansion
 cd %~dp0
 
 dotnet restore
-msbuild LiveFrame.csproj -t:Build -p:Configuration=Release
+dotnet publish --runtime win-x64 --configuration Release --self-contained false
+
